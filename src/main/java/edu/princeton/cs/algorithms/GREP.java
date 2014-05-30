1a571e9a1dd0401d837117275f5de00c6a762c2b
Peter Reutemann <fracpete@gmail.com>
2014-05-30 16:48:26 +1200

    initial import of mavenized Princeton's Algorithms example code

---
+++ src/main/java/edu/princeton/cs/algorithms/GREP.java
@@ -0,0 +1,44 @@
+package edu.princeton.cs.algorithms;
+
+import edu.princeton.cs.introcs.StdIn;
+import edu.princeton.cs.introcs.StdOut;
+
+/*************************************************************************
+ *  Compilation:  javac GREP.java
+ *  Execution:    java GREP regexp < input.txt
+ *  Dependencies: NFA.java
+ *  Data files:   http://algs4.cs.princeton.edu/54regexp/tinyL.txt
+ *
+ *  This program takes an RE as a command-line argument and prints
+ *  the lines from standard input having some substring that
+ *  is in the language described by the RE. 
+ *
+ *  % more tinyL.txt
+ *  AC
+ *  AD
+ *  AAA
+ *  ABD
+ *  ADD
+ *  BCD
+ *  ABCCBD
+ *  BABAAA
+ *  BABBAAA
+ *
+ *  %  java GREP "(A*B|AC)D" < tinyL.txt
+ *  ABD
+ *  ABCCBD
+ *
+ *************************************************************************/
+
+public class GREP {
+    public static void main(String[] args) { 
+        String regexp = "(.*" + args[0] + ".*)";
+        NFA nfa = new NFA(regexp);
+        while (StdIn.hasNextLine()) { 
+            String txt = StdIn.readLine();
+            if (nfa.recognizes(txt)) {
+                StdOut.println(txt);
+            }
+        }
+    } 
+} 
